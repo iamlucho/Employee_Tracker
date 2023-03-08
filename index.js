20729066e14f54ed8f0a10fc8148374766baaa88
Luciano Aguirre <luciano.aguirre@collins.com>
2023-03-07 23:05:10 -0500

    Added prompt options to index.js for user input

--- index.js
+++ index.js
@@ -3,3 +3,72 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
+// create the MySQL connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'employees_db'
+  });
+  
+  // connect to the MySQL server
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    // start the application after the connection is established
+    start();
+  });
+  
+  // start the application
+  function start() {
+    // use inquirer to prompt the user for the desired action
+    inquirer
+      .prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role',
+          'Exit'
+        ]
+      })
+      .then((answer) => {
+        // based on the user's choice, call the corresponding function
+        switch (answer.action) {
+          case 'View all departments':
+            viewAllDepartments();
+            break;
+          case 'View all roles':
+            viewAllRoles();
+            break;
+          case 'View all employees':
+            viewAllEmployees();
+            break;
+          case 'Add a department':
+            addDepartment();
+            break;
+          case 'Add a role':
+            addRole();
+            break;
+          case 'Add an employee':
+            addEmployee();
+            break;
+          case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+          case 'Exit':
+            connection.end();
+            break;
+          default:
+            console.log(`Invalid action: ${answer.action}`);
+            break;
+        }
+      });
+  }
